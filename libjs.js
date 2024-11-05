61c0e33ac18c3b5f15c126b63b76e6109d3b5422
BuggieBot <buggiebot@serenityos.org>
2024-11-05 02:59:02 +0000

    Deploying to libjs-wasm from @ SerenityOS/serenity@5bad39d71f038b1e72f90efe5d5bf74cf6e5c2f6 🚀

--- libjs.js
+++ libjs.js
@@ -28,7 +28,7 @@ if (Module['ENVIRONMENT']) {
 
 // --pre-jses are emitted after the Module integration code, so that they can
 // refer to Module (if they choose; they can also define Module)
-// include: /home/runner/work/_temp/dde52266-0b43-4fec-8969-cd43e7905f38/emsdk-main/upstream/emscripten/src/emrun_prejs.js
+// include: /home/runner/work/_temp/671354ad-3f13-4228-8b40-75d2419b5dc5/emsdk-main/upstream/emscripten/src/emrun_prejs.js
 /**
  * @license
  * Copyright 2013 The Emscripten Authors
@@ -49,7 +49,7 @@ if (typeof window == 'object') {
     Module['arguments'] = [];
   }
 }
-// end include: /home/runner/work/_temp/dde52266-0b43-4fec-8969-cd43e7905f38/emsdk-main/upstream/emscripten/src/emrun_prejs.js
+// end include: /home/runner/work/_temp/671354ad-3f13-4228-8b40-75d2419b5dc5/emsdk-main/upstream/emscripten/src/emrun_prejs.js
 
 
 // Sometimes an existing Module object exists with properties
@@ -4882,7 +4882,7 @@ run();
 
 // end include: postamble.js
 
-// include: /home/runner/work/_temp/dde52266-0b43-4fec-8969-cd43e7905f38/emsdk-main/upstream/emscripten/src/emrun_postjs.js
+// include: /home/runner/work/_temp/671354ad-3f13-4228-8b40-75d2419b5dc5/emsdk-main/upstream/emscripten/src/emrun_postjs.js
 /**
  * @license
  * Copyright 2013 The Emscripten Authors
@@ -4978,6 +4978,6 @@ if (typeof window == "object" && (typeof ENVIRONMENT_IS_PTHREAD == 'undefined' |
     emrun_register_handlers();
   }
 }
-// end include: /home/runner/work/_temp/dde52266-0b43-4fec-8969-cd43e7905f38/emsdk-main/upstream/emscripten/src/emrun_postjs.js
+// end include: /home/runner/work/_temp/671354ad-3f13-4228-8b40-75d2419b5dc5/emsdk-main/upstream/emscripten/src/emrun_postjs.js
 
-Module.SERENITYOS_COMMIT = "fbe99575e000af2bde3647879d730ea0293edf11";
+Module.SERENITYOS_COMMIT = "5bad39d71f038b1e72f90efe5d5bf74cf6e5c2f6";
