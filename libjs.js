24728178f3ca14b42e6598684edb88e27777b190
BuggieBot <buggiebot@serenityos.org>
2024-11-07 00:40:30 +0000

    Deploying to libjs-wasm from @ SerenityOS/serenity@6a96e1c23f9e407bf5780dcbcbe2730cb86de2cc 🚀

--- libjs.js
+++ libjs.js
@@ -28,7 +28,7 @@ if (Module['ENVIRONMENT']) {
 
 // --pre-jses are emitted after the Module integration code, so that they can
 // refer to Module (if they choose; they can also define Module)
-// include: /home/runner/work/_temp/6b816325-ec4b-4009-9eb8-efeea1ea1c19/emsdk-main/upstream/emscripten/src/emrun_prejs.js
+// include: /home/runner/work/_temp/f82d937a-f8b8-4962-9983-14d7cd996cbb/emsdk-main/upstream/emscripten/src/emrun_prejs.js
 /**
  * @license
  * Copyright 2013 The Emscripten Authors
@@ -49,7 +49,7 @@ if (typeof window == 'object') {
     Module['arguments'] = [];
   }
 }
-// end include: /home/runner/work/_temp/6b816325-ec4b-4009-9eb8-efeea1ea1c19/emsdk-main/upstream/emscripten/src/emrun_prejs.js
+// end include: /home/runner/work/_temp/f82d937a-f8b8-4962-9983-14d7cd996cbb/emsdk-main/upstream/emscripten/src/emrun_prejs.js
 
 
 // Sometimes an existing Module object exists with properties
@@ -4882,7 +4882,7 @@ run();
 
 // end include: postamble.js
 
-// include: /home/runner/work/_temp/6b816325-ec4b-4009-9eb8-efeea1ea1c19/emsdk-main/upstream/emscripten/src/emrun_postjs.js
+// include: /home/runner/work/_temp/f82d937a-f8b8-4962-9983-14d7cd996cbb/emsdk-main/upstream/emscripten/src/emrun_postjs.js
 /**
  * @license
  * Copyright 2013 The Emscripten Authors
@@ -4978,6 +4978,6 @@ if (typeof window == "object" && (typeof ENVIRONMENT_IS_PTHREAD == 'undefined' |
     emrun_register_handlers();
   }
 }
-// end include: /home/runner/work/_temp/6b816325-ec4b-4009-9eb8-efeea1ea1c19/emsdk-main/upstream/emscripten/src/emrun_postjs.js
+// end include: /home/runner/work/_temp/f82d937a-f8b8-4962-9983-14d7cd996cbb/emsdk-main/upstream/emscripten/src/emrun_postjs.js
 
-Module.SERENITYOS_COMMIT = "a34248f1b20a85eaf022b35b7ef19c1c1afc536e";
+Module.SERENITYOS_COMMIT = "6a96e1c23f9e407bf5780dcbcbe2730cb86de2cc";
