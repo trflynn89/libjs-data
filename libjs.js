15b2f82d3f9b468f2959137afba6c8281bf7b3ae
BuggieBot <buggiebot@serenityos.org>
2025-03-16 10:34:41 +0000

    Deploying to libjs-wasm from @ SerenityOS/serenity@1af69c37823fb460743e3ff470d99196315027ab 🚀

--- libjs.js
+++ libjs.js
@@ -28,7 +28,7 @@ if (Module['ENVIRONMENT']) {
 
 // --pre-jses are emitted after the Module integration code, so that they can
 // refer to Module (if they choose; they can also define Module)
-// include: /home/runner/work/_temp/0f83445a-08e3-4697-9378-7ebc960b6212/emsdk-main/upstream/emscripten/src/emrun_prejs.js
+// include: /home/runner/work/_temp/e05f5bc1-9e1e-48bf-bd34-0690f870a92c/emsdk-main/upstream/emscripten/src/emrun_prejs.js
 /**
  * @license
  * Copyright 2013 The Emscripten Authors
@@ -49,7 +49,7 @@ if (typeof window == 'object') {
     Module['arguments'] = [];
   }
 }
-// end include: /home/runner/work/_temp/0f83445a-08e3-4697-9378-7ebc960b6212/emsdk-main/upstream/emscripten/src/emrun_prejs.js
+// end include: /home/runner/work/_temp/e05f5bc1-9e1e-48bf-bd34-0690f870a92c/emsdk-main/upstream/emscripten/src/emrun_prejs.js
 
 
 // Sometimes an existing Module object exists with properties
@@ -5036,7 +5036,7 @@ run();
 
 // end include: postamble.js
 
-// include: /home/runner/work/_temp/0f83445a-08e3-4697-9378-7ebc960b6212/emsdk-main/upstream/emscripten/src/emrun_postjs.js
+// include: /home/runner/work/_temp/e05f5bc1-9e1e-48bf-bd34-0690f870a92c/emsdk-main/upstream/emscripten/src/emrun_postjs.js
 /**
  * @license
  * Copyright 2013 The Emscripten Authors
@@ -5132,6 +5132,6 @@ if (typeof window == "object" && (typeof ENVIRONMENT_IS_PTHREAD == 'undefined' |
     emrun_register_handlers();
   }
 }
-// end include: /home/runner/work/_temp/0f83445a-08e3-4697-9378-7ebc960b6212/emsdk-main/upstream/emscripten/src/emrun_postjs.js
+// end include: /home/runner/work/_temp/e05f5bc1-9e1e-48bf-bd34-0690f870a92c/emsdk-main/upstream/emscripten/src/emrun_postjs.js
 
-Module.SERENITYOS_COMMIT = "276655b4ce9381c7e95159a4390a3c532cca6c72";
+Module.SERENITYOS_COMMIT = "1af69c37823fb460743e3ff470d99196315027ab";
