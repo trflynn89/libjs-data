69133653377f29247c9d524ce9ddf728cbfc6933
BuggieBot <buggiebot@serenityos.org>
2025-07-08 06:15:19 +0000

    Deploying to libjs-wasm from @ SerenityOS/serenity@ea84d58a5ad00504a0c4f0875d3cb4aed83b7291 🚀

--- libjs.js
+++ libjs.js
@@ -28,7 +28,7 @@ if (Module['ENVIRONMENT']) {
 
 // --pre-jses are emitted after the Module integration code, so that they can
 // refer to Module (if they choose; they can also define Module)
-// include: /home/runner/work/_temp/ebc23e51-c976-4dfe-ad02-ec4f2f0b40a0/emsdk-main/upstream/emscripten/src/emrun_prejs.js
+// include: /home/runner/work/_temp/b9143718-7447-470d-bea3-b0271f3ffd38/emsdk-main/upstream/emscripten/src/emrun_prejs.js
 /**
  * @license
  * Copyright 2013 The Emscripten Authors
@@ -49,7 +49,7 @@ if (typeof window == 'object') {
     Module['arguments'] = [];
   }
 }
-// end include: /home/runner/work/_temp/ebc23e51-c976-4dfe-ad02-ec4f2f0b40a0/emsdk-main/upstream/emscripten/src/emrun_prejs.js
+// end include: /home/runner/work/_temp/b9143718-7447-470d-bea3-b0271f3ffd38/emsdk-main/upstream/emscripten/src/emrun_prejs.js
 
 
 // Sometimes an existing Module object exists with properties
@@ -5036,7 +5036,7 @@ run();
 
 // end include: postamble.js
 
-// include: /home/runner/work/_temp/ebc23e51-c976-4dfe-ad02-ec4f2f0b40a0/emsdk-main/upstream/emscripten/src/emrun_postjs.js
+// include: /home/runner/work/_temp/b9143718-7447-470d-bea3-b0271f3ffd38/emsdk-main/upstream/emscripten/src/emrun_postjs.js
 /**
  * @license
  * Copyright 2013 The Emscripten Authors
@@ -5132,6 +5132,6 @@ if (typeof window == "object" && (typeof ENVIRONMENT_IS_PTHREAD == 'undefined' |
     emrun_register_handlers();
   }
 }
-// end include: /home/runner/work/_temp/ebc23e51-c976-4dfe-ad02-ec4f2f0b40a0/emsdk-main/upstream/emscripten/src/emrun_postjs.js
+// end include: /home/runner/work/_temp/b9143718-7447-470d-bea3-b0271f3ffd38/emsdk-main/upstream/emscripten/src/emrun_postjs.js
 
-Module.SERENITYOS_COMMIT = "d884ec0ae069eca307b516b4e16c39cf590c1317";
+Module.SERENITYOS_COMMIT = "ea84d58a5ad00504a0c4f0875d3cb4aed83b7291";
